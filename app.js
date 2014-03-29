7a6a330f20ceba5fc1da10df356fb24f1e474fdf
Ajay Singh <meajaysingh@hotmail.com>
2014-03-29 18:09:56 +0530

    8 sec to refresh

--- app.js
+++ app.js
@@ -130,7 +130,7 @@ io.sockets.on('connection', function (socket) {
                  
  }
 
- },17000);
+ },8000);
     socket.on('disconnect', function () {
     console.log('@@@@@@@-------------Disconnecting from sockets');
     clearInterval(intervalid);
